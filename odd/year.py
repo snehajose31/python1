f73c0f98bdebc8024392241e64016ed3737bc37d
snehajose31 <120885046+snehajose31@users.noreply.github.com>
2023-01-20 07:04:11 +0530

    Add files via upload

---
+++ odd/year.py
@@ -0,0 +1,7 @@
+year1=int(input("enter the current year"))
+year2=int(input("enter the last year"))
+print("leap years are")
+for year in range(year1,year2):
+    if(year%4==0)and(year%100!=0)or(year%400==0):
+        print(year)
+
