187cbda2b3a834ab29bc587adbe702883f42cc82
snehajose31 <120885046+snehajose31@users.noreply.github.com>
2023-02-24 14:06:36 +0530

    Add files via upload

---
+++ sqr.py
@@ -0,0 +1,4 @@
+l=int(input("enter the limited value until code should be generated"))
+a=[]
+a=[i*i for i in range(l+l)]
+print("the list of square of number",a)
---
+++ vowel.py
@@ -0,0 +1,3 @@
+list=input("enter the word")
+letter=[x for x in list if x=='a'or x=='e'or x=='i' or x=='o'or x=='u']
+print(letter)
